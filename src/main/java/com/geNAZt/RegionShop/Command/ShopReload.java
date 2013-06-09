d867495370e195bcbfed279c37f2599880b88286
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-06-09 10:08:09 +0200

    Message and /shop fix

--- src/main/java/com/geNAZt/RegionShop/Command/ShopReload.java
+++ src/main/java/com/geNAZt/RegionShop/Command/ShopReload.java
@@ -3,6 +3,9 @@ package com.geNAZt.RegionShop.Command;
 
 import com.geNAZt.RegionShop.RegionShopPlugin;
 import com.geNAZt.RegionShop.Storages.ListStorage;
+import com.geNAZt.RegionShop.Util.Chat;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,9 +22,11 @@ public class ShopReload {
     }
 
     @SuppressWarnings("unchecked")
-    public boolean execute() {
+    public boolean execute(Player p) {
         ListStorage.reload();
         plugin.reloadConfig();
+
+        p.sendMessage(Chat.getPrefix() + ChatColor.YELLOW + "Reloaded " + ChatColor.GOLD + "Config/ShopList");
 
         return false;
     }
