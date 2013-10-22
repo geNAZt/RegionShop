e75ba57bf3c6d5eb16f85c7f11de3c448d0e04d6
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-10-22 20:00:58 +0200

    Pretend CustomerSign Showcases from being picked up.

--- src/main/java/com/geNAZt/RegionShop/Listener/PretendDisplaysToPickup.java
+++ src/main/java/com/geNAZt/RegionShop/Listener/PretendDisplaysToPickup.java
@@ -36,7 +36,7 @@ public class PretendDisplaysToPickup implements Listener {
                 findUnique();
 
         if(chest != null) {
-            ,.setCancelled(true);
+            event.setCancelled(true);
         }
     }
 }
