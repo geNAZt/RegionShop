e1e06b1b774b4fcbbde7fcaa2e249dcd8fe47ec3
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-10-30 11:45:21 +0100

    Removed the Plugin load as last code since Bukkit has something called "loadbefore" in plugin.yml

--- src/main/java/net/cubespace/RegionShop/Bukkit/Plugin.java
+++ src/main/java/net/cubespace/RegionShop/Bukkit/Plugin.java
@@ -1,12 +1,7 @@
 package net.cubespace.RegionShop.Bukkit;
 
 import net.cubespace.RegionShop.Data.Parser.ItemDB;
-import net.cubespace.RegionShop.Util.Logger;
-import net.cubespace.RegionShop.Util.Version;
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -19,50 +14,14 @@ public class Plugin extends JavaPlugin implements Listener {
     /* This variable holds the Instance of the RegionShop which has been initialized */
     private static Plugin instance;
 
-    private Integer amountOfOtherPlugins = 0;
-    private Integer amoutOfEnabledPlugins = 0;
-
     /**
      * This function gets called when the Bukkit Plugin Manager wants to init the Plugin
      */
     public void onEnable() {
         instance = this;
-        amountOfOtherPlugins = Bukkit.getPluginManager().getPlugins().length;
 
-        Logger.info("Running on MC " + Version.getMcVersion());
-        Logger.info("Delaying start of the Plugin so other Plugins have the chance to register");
-        Logger.info("Detected " + amountOfOtherPlugins + " other Plugins. Waiting for them to enable");
-
-        for(org.bukkit.plugin.Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
-            if(plugin.isEnabled()) {
-                amoutOfEnabledPlugins++;
-            }
-        }
-
-        if(amountOfOtherPlugins.equals(amoutOfEnabledPlugins)) {
-            Logger.info("Starting RegionShop");
-            start();
-        }
-    }
-
-    /**
-     * Start the Plugin after all other Plugins have loaded
-     */
-    private void start() {
+        //Start the ItemDB
         new ItemDB();
-    }
-
-    /**
-     * If a new Plugin gets enabled
-     */
-    @EventHandler
-    public void onPluginEnable(PluginEnableEvent event) {
-        amoutOfEnabledPlugins++;
-
-        if(amountOfOtherPlugins.equals(amoutOfEnabledPlugins)) {
-            Logger.info("Starting RegionShop");
-            start();
-        }
     }
 
     /**
